e1ff99e4d922c928a82b2b9c28e37a8489879d27
sj <sjingye@163.com>
2019-12-25 17:23:21 +0800

    login

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import useAuth from '../../util/auth';
+import React, { useState } from 'react';
 
 export default function Login () {
-    useAuth()
+    const [username, setUsername ] = useState('')
+    const [password, setPassword ] = useState('')
+    function handleUsernameChange (e) {
+        setUsername(e.target.value)
+        let msg = ''
+        if (!/^\w+$/.test(e.target.value)) {
+            msg = '请输入字母、数字或者下划线'
+            alert(msg)
+        }
+        return Boolean(msg)
+    }
+    function handlePasswordChange (e) {
+        setPassword(e.target.value)
+        let msg = ''
+        if (!/^\w+$/.test(e.target.value)) {
+            msg = '请输入字母、数字或者下划线'
+            alert(msg)
+        }
+        return Boolean(msg)
+    }
+    function handleLogin() {
+        if (handleUsernameChange || handlePasswordChange) {
+            return;
+        }
+        console.log(username, password)
+    }
     return (
         <div className="login-wrapper">
-            登录
+            <input type="text" value={username} onChange={handleUsernameChange} />
+            <input type="password" value={password} onChange={handlePasswordChange} />
+            <button onClick={handleLogin}>登录</button>
         </div>
     )
 }
